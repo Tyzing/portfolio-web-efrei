8f4cc228b884fa7e08ae1bac2c5ece2e1e25f5ea
Bastien <bastiendubile@gmail.com>
2026-05-20 14:42:24 +0200

    ajout du squelette des composants

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import About from "./About";
+import Button from "./Button";
+import Footer from "./Footer";
+import Heros from "./Heros";
+import ItemProject from "./ItemProject";
+import ListProject from "./ListProject";
+import NavBar from "./ListProject";
+
+export {
+    About,
+    Button,
+    Footer,
+    Heros,
+    ItemProject,
+    ListProject,
+    NavBar
+}
