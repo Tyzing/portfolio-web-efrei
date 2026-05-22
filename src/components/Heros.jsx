40a31001c3191839b08a820931426caa7855478d
Bastien <bastiendubile@gmail.com>
2026-05-22 09:43:30 +0200

    Ajout de nouvelle police + tittle du Heros

--- src/components/Heros.jsx
+++ src/components/Heros.jsx
@@ -1,6 +1,11 @@
 function Heros() {
   return (
-    <div>
+    <div className="w-[33%] flex flex-col gap-[12px] mt-[64px]">
+      <p className="font-nunito text-[20px] text-yellow">DEVELOPPEUR WEB</p>
+      <div className="flex flex-col gap-[32px]">
+        <h1 className="font-poppins font-bold text-[64px] leading-[1.2] text-charcoal m-0">Hello,  my name is Bastien Dubile</h1>
+        <p className="font-raleway text-[24px] text-gray">Short text with details about you, what you do or your professional career. You can add more information on the about page.</p>
+      </div>
     </div>
   )
 }
