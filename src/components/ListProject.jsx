f7b0b1e94f9aef704c1e2e4974f4677e2e05a209
Bastien <bastiendubile@gmail.com>
2026-05-22 16:41:10 +0200

    Ajout component ItemProject

--- src/components/ListProject.jsx
+++ src/components/ListProject.jsx
@@ -1,8 +1,16 @@
 import TitleSection from "./TitleSection"
+import ItemProject from "./ItemProject"
 function ListProject({ title }) {
   return (
-    <div className="flex flex-col mt-[200px]">
+    <div className="flex flex-col mt-[200px] gap-[80px]">
       <TitleSection title={title} />
+      <ItemProject
+        titleProject="Project Name"
+        descriptionProject="I created this personal project in order to show how to create an interface in Figma using a portfolio as an example."
+        linkProject="https://example.com"
+        imageSrc="/my-image.jpg"
+        imageRight={true}
+      />
     </div>
   )
 }
