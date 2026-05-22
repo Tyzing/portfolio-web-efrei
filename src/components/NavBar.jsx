b6fdcfbd1676d79437f23443a4fdf2f9bac46f36
Bastien <bastiendubile@gmail.com>
2026-05-22 10:56:32 +0200

    NavBar ajout soulignement lors du hover

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,17 +6,17 @@ function NavBar({ name }) {
       </span>
       <ul className="flex gap-[48px] list-none m-0 p-0">
         <li>
-          <a href="#about" className="font-raleway text-[18px] text-charcoal no-underline">
+          <a href="#about" className="font-raleway text-[18px] text-charcoal no-underline hover:underline">
             About
           </a>
         </li>
         <li>
-          <a href="#projects" className="font-raleway text-[18px] text-charcoal no-underline">
+          <a href="#projects" className="font-raleway text-[18px] text-charcoal no-underline hover:underline">
             Projects
           </a>
         </li>
         <li>
-          <a href="#contacts" className="font-raleway text-[18px] text-charcoal no-underline">
+          <a href="#contacts" className="font-raleway text-[18px] text-charcoal no-underline hover:underline">
             Contacts
           </a>
         </li>
