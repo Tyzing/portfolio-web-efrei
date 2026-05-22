37e00eb6a483c32c748f2dd38aa2315a236a2c59
Bastien <bastiendubile@gmail.com>
2026-05-22 10:28:08 +0200

    création des buttons et finalisation du Heros

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,16 @@
-function Button() {
+function Button({ text, borderColor, backgroundColor, textColor }) {
   return (
-    <div>
-    </div>
-  )
+    <button
+      className="font-roboto font-medium text-[18px] rounded-[8px] py-[8px] px-[24px] border-2 cursor-pointer shadow-lg"
+      style={{
+        borderColor: borderColor,
+        backgroundColor: backgroundColor,
+        color: textColor,
+      }}
+    >
+      {text}
+    </button>
+  );
 }
 
-export default Button
+export default Button;
