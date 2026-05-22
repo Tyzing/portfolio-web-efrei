d270fcc35c5c02ca11f7c14180f4282fae164a27
Bastien <bastiendubile@gmail.com>
2026-05-22 10:28:08 +0200

    création des buttons et finalisation du Heros

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const name = "Bastien Dubile";
 
 function App() {
   return (
-    <div className="bg-[url('/image-accueil.webp')] bg-no-repeat bg-right-top bg-auto min-h-screen">
+    <div className="bg-[url('/image-accueil.webp')] bg-no-repeat bg-right-top bg-[size:50%_auto] min-h-screen">
       <div className="mx-[8%]">
         <Header name={name} />
         <Heros name={name} />
--- src/components/Heros.jsx
+++ src/components/Heros.jsx
@@ -1,3 +1,5 @@
+import Button from "./Button"
+
 function Heros({ name }) {
   return (
     <div className="w-[33%] flex flex-col gap-[12px] mt-[64px]">
@@ -5,6 +7,10 @@ function Heros({ name }) {
       <div className="flex flex-col gap-[32px]">
         <h1 className="font-poppins font-bold text-[64px] leading-[1.2] text-charcoal m-0">Hello,  my name is {name}</h1>
         <p className="font-raleway text-[24px] text-gray">Short text with details about you, what you do or your professional career. You can add more information on the about page.</p>
+        <div className="flex gap-[12px]">
+          <Button text="Projects" backgroundColor="var(--color-yellow)" borderColor="var(--color-yellow)" textColor="var(--color-charcoal)" />
+          <Button text="LinkedIn" backgroundColor="transparent" borderColor="var(--color-charcoal)" textColor="var(--color-charcoal)" />
+        </div>
       </div>
     </div>
   )
