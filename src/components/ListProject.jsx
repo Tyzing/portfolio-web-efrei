168ff810033627110000376f78ca3699d0a2120b
Bastien <bastiendubile@gmail.com>
2026-05-23 10:52:14 +0200

    Structuration ListProject

--- src/components/ListProject.jsx
+++ src/components/ListProject.jsx
@@ -1,16 +1,22 @@
 import TitleSection from "./TitleSection"
 import ItemProject from "./ItemProject"
+import projects from "../data/projects.json"
+
 function ListProject({ title }) {
   return (
-    <div className="flex flex-col mt-[200px] gap-[80px]">
+    <div className="flex flex-col mt-[200px] gap-[80px] mx-[8%]">
       <TitleSection title={title} />
-      <ItemProject
-        titleProject="Project Name"
-        descriptionProject="I created this personal project in order to show how to create an interface in Figma using a portfolio as an example."
-        linkProject="https://example.com"
-        imageSrc="/my-image.jpg"
-        imageRight={true}
-      />
+      {projects.map((project, index) => (
+        <ItemProject
+          key={index}
+          titleProject={project.titleProject}
+          descriptionProject={project.descriptionProject}
+          linkProject={project.linkProject}
+          imageSrc={project.imageSrc}
+          textButton={project.textButton}
+          imageRight={index % 2 !== 0}
+        />
+      ))}
     </div>
   )
 }
