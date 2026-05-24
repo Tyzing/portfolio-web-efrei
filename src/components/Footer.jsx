f8a1b17fefc77ff11ce8e9d69442868fbba22602
Bastien <bastiendubile@gmail.com>
2026-05-24 18:17:03 +0200

    Création du composant Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,18 @@
 function Footer() {
   return (
-    <div>
+    <div className="flex flex-col items-center gap-[32px] mt-[100px] mb-[50px]">
+      <div className="flex items-center gap-[24px]">
+        <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+          <img src="/InstagramLogo.svg" alt="Instagram" width={37} />
+        </a>
+        <a href="https://www.linkedin.com/in/bastien-dubile/" target="_blank" rel="noopener noreferrer">
+          <img src="/LinkedInLogo.svg" alt="LinkedIn" width={38} />
+        </a>
+        <a href="mailto:">
+          <img src="/MailLogo.svg" alt="Mail" width={40} />
+        </a>
+      </div>
+      <p className="font-nunito text-[16px] text-gray">Bastien Dubile 2026</p>
     </div>
   )
 }
